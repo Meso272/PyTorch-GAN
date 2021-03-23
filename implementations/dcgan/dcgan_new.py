cac70e82514f33f0f5e73f370f30065d9bf57bfc
meso272 <liujinyang1201@126.com>
2021-03-23 00:14:43 -0700

    dd

--- implementations/dcgan/dcgan_new.py
+++ implementations/dcgan/dcgan_new.py
@@ -57,9 +57,10 @@ class Generator(nn.Module):
         
         modules = []
         if opt.hidden_dims is None:
-            hidden_dims = [32, 64, 128, 256, 512]
+            hidden_dims = [32, 64, 128, 256]
         else:
             hidden_dims=opt.hidden_dims
+        print(hidden_dims)
         self.last_fm_nums=hidden_dims[-1]
         self.last_fm_size=int( opt.img_size/(2**len(hidden_dims)) )
         # Build Encoder
