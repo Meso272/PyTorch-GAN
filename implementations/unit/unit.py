3d9ead1f0669012982a260216c0d2b20dc6d0805
meso272 <liujinyang1201@126.com>
2021-05-27 22:21:08 -0700

    1

--- implementations/unit/unit.py
+++ implementations/unit/unit.py
@@ -35,7 +35,7 @@ parser.add_argument("--img_height", type=int, default=320, help="size of image h
 parser.add_argument("--img_width", type=int, default=256, help="size of image width")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving generator samples")
-parser.add_argument("--checkpoint_interval", type=int, default=25, help="interval between saving model checkpoints")
+parser.add_argument("--checkpoint_interval", type=int, default=10, help="interval between saving model checkpoints")
 parser.add_argument("--n_downsample", type=int, default=2, help="number downsampling layers in encoder")
 parser.add_argument("--dim", type=int, default=64, help="number of filters in first encoder layer")
 opt = parser.parse_args()
